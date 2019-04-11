f83537eb7a40ab7cdc68d4293a5b9650fb6bbea7
Jithin J Kumar <jithinjkumar@gmail.com>
2019-04-11 19:36:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
-rom flask import Flask,render_template,url_for,request,redirect
+from flask import Flask,render_template,url_for,request,redirect
+import time
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", values ='')
+	return render_template("index.html", values ='', meeting = '', timer='')
 
 @app.route("/create",methods=["POST"])
 def create():
-    sprint = request.form.get("sprint")
-    plan = request.form.get("plan")
-    code = request.form.get("code")
-    review = request.form.get("review")
-    retro = request.form.get("retro")
-    return render_template("index.html", values = [sprint,plan,code,review,retro])
+	sprint = request.form.get("sprint")
+	plan = request.form.get("plan")
+	code = request.form.get("code")
+	review = request.form.get("review")
+	retro = request.form.get("retro")
+	for item in  ['Planning Meeting','Code Meeting','Review Meeting','Retro Meeting']:		
+		meeting = item
+		if meeting == 'Planning Meeting': 
+			duration = int(plan) 
+		elif meeting == 'Code Meeting':
+			duration = int(code)
+		elif meeting == 'Review Meeting':
+			duration = int(review)
+		elif meeting == 'Retro Meeting':
+			duration = int(retro)
 
-if name == "main":
-    app.run(debug=True)
+		return render_template("index.html", values = sprint, meeting = meeting, timer=duration)
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
